de456abb4e0be0c234110df9c9193e7f3aad1b88
mohamedabdelsalam9 <mabdelsalam944@gmail.com>
2021-01-18 14:44:29 -0500

    package files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Mohamed Abdelsalam, Mojtaba Faramarzi, Shagun Sodhani, Sarath
 author = 'Mohamed Abdelsalam, Mojtaba Faramarzi, Shagun Sodhani, Sarath Chandar (Chandar-Lab/Mila)'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
